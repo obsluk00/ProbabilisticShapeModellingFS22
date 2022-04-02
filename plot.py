3a691976da7d616d2e668b2c1034e2acf8e78174
luka.obser@unibas.ch <luka.obser@stud.unibas.ch>
2022-04-02 16:30:43 +0200

    added plotting script and scatterplot of the data from exercise 1.3

---
+++ plot.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("project-data/distances.csv")
+df.plot(kind='scatter', x=' length', y=' width').get_figure().savefig('measurements.png')
