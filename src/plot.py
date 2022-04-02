39c5fcee1d2ae9799d02cfdc1a96575b9a35d7c7
luka.obser@unibas.ch <luka.obser@stud.unibas.ch>
2022-04-02 16:41:55 +0200

    added calculations and note of mean and variance of measurements

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("../project-data/distances.csv")
+df = pd.read_csv("F:/Databases/ProbabilisticShapeModellingFS22/project-data/distances.csv")
 df.plot(kind='scatter', x=' length', y=' width').get_figure().savefig('measurements.png')
+
+# mean length 420.440909, mean width 60.529105
+print(df.mean())
+# length squared variance 848.037875, width squared variance 18.477001
+print(df.var())
