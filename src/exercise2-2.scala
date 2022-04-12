7b2f9f959ea8f85fa78027285de8fd99f7dd2244
luka.obser@unibas.ch <luka.obser@stud.unibas.ch>
2022-04-12 15:34:22 +0200

    finished 2.2

---
+++ src/exercise2-2.scala
@@ -0,0 +1,93 @@
+//Load the reference mesh
+val reference = MeshIO.readMesh(new File("datasets/project-data/reference-mesh/reference.stl")).get
+show(reference,"reference")
+
+//Initialize mean of GP as zero mean
+val zeroMean = VectorField(RealSpace[_3D], (pt:Point[_3D]) => Vector(0,0,0))
+
+//Build the covariance matrix using a finite rank representation since the resolution of the given mesh is too high (OutOfMemoryError)
+
+//TODO: play around with kernel for better results
+case class XmirroredKernel(ker : PDKernel[_3D]) extends PDKernel[_3D] {
+  override def domain = RealSpace[_3D]
+  override def k(x: Point[_3D], y: Point[_3D]) = ker(Point(x(0) * -1f ,x(1), x(2)), y)
+}
+
+case class MyKernel(sigma: Double) extends PDKernel[_3D] {
+  override def domain = RealSpace[_3D]
+  override def k(x: Point[_3D], y: Point[_3D]) = {
+  exp(- (x.toVector dot y.toVector) / (2 * sigma * sigma))
+  }
+}
+
+case class ElongingKernel(ker : PDKernel[_3D]) extends PDKernel[_3D] {
+  override def domain = RealSpace[_3D]
+  override def k(x: Point[_3D], y: Point[_3D]) = ker(Point(x(0) * 0f ,x(1) * 10f, x(2) * 0f), y)
+}
+
+def MyKernel(ker : PDKernel[_3D]) : MatrixValuedPDKernel[_3D,_3D] = {
+   val xmirrored = XmirroredKernel(ker)
+   val elonging = ElongingKernel(ker)
+   val k1 = DiagonalKernel(ker)
+   val k2 = DiagonalKernel(xmirrored * -1f, xmirrored, xmirrored)
+   val k3 = DiagonalKernel(elonging * 0f, elonging * 1f, elonging * 0f)
+   k1 + k2
+}
+
+val sigma :Double = 100.0
+val s :Double = 10.0
+
+val myKernel = new MyKernel(sigma)
+val simGauss = MyKernel(myKernel * s)
+
+val gp = GaussianProcess(zeroMean, simGauss)
+
+val sampler = RandomMeshSampler3D(
+  reference,
+  numberOfPoints = 300,
+  seed = 42)
+
+val lowRankGP = LowRankGaussianProcess.approximateGP(gp, sampler, numBasisFunctions = 10)
+
+//creates mesh from deformationfield
+def transformFromDefField(defField : DiscreteVectorField[_3D, _3D]) =   (pt : Point[_3D]) => {
+  pt+defField(reference.findClosestPoint(pt).id)
+}
+
+//Take and Visualize samples
+remove("sampleField")
+remove("sampleMesh")
+val sample = lowRankGP.sampleAtPoints(reference)
+show(sample, "sampleField")
+show(reference.transform(transformFromDefField(sample)), "sampleMesh")
+
+// Class to store the measurements
+case class Measurement(id : String, length : Double, width : Double)
+
+// create empty sequence which will store measurements
+var ourMeasurements = Seq[Measurement]()
+
+// load the landmarks, calculate distances, add them to the sequence
+(0 until 50).foreach{i :Int =>
+  val referenceLandmarks = LandmarkIO.readLandmarksJson[_3D](new File("datasets/project-data/reference-landmarks/reference.json")).get
+  val sample = lowRankGP.sampleAtPoints(reference)
+  val landmarks = referenceLandmarks.map(pId => Landmark(s"${pId.id}", transformFromDefField(sample)(pId.point)))
+  val length : Vector[_3D] = landmarks(2).point - landmarks(5).point
+  val width : Vector [_3D] = landmarks(3).point - landmarks(4).point
+  var ourMeasurement = new Measurement(i.toString, length.norm, width.norm)
+  ourMeasurements = ourMeasurements :+ ourMeasurement
+}
+
+// method to save the sequence of measurements
+def writeCSV(csvFile : java.io.File, measurements :Seq[Measurement]) : Unit = {
+  import java.io.PrintWriter
+  val printWriter = new PrintWriter(csvFile)
+  printWriter.write("id, length, width\n")
+  for (measurement <- measurements) {
+    printWriter.write(s"${measurement.id},${measurement.length},${measurement.width}\n")
+  }
+  printWriter.close()
+}
+
+// call to aforementioned method
+writeCSV(new File("datasets/project-data/distancesGPModel.csv"), ourMeasurements)
--- src/plot.py
+++ src/plot.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("F:/Databases/ProbabilisticShapeModellingFS22/project-data/distances.csv")
-df.plot(kind='scatter', x=' length', y=' width').get_figure().savefig('measurements.png')
+referenceDF = pd.read_csv("F:/Databases/ProbabilisticShapeModellingFS22/project-data/distances.csv")
+gpDF = pd.read_csv("F:/Databases/ProbabilisticShapeModellingFS22/project-data/distancesGPModel.csv")
+
+ax = referenceDF.plot(kind='scatter', x=' length', y=' width', color='Green')
+gpDF.plot(ax=ax, kind='scatter', x=' length', y=' width', color='Orange').get_figure().savefig('measurements.png')
 
 # mean length 420.440909, mean width 60.529105
-print(df.mean())
+print(referenceDF.mean())
 # length squared variance 848.037875, width squared variance 18.477001
-print(df.var())
+print(referenceDF.var())
+
+# mean length 425.530941, 70.087738 mean width
+print(gpDF.mean())
+# length squared variance 376.879229, width squared variance 5.400647
+print(gpDF.var())
